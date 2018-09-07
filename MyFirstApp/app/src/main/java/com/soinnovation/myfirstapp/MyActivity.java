ab5331bc338f7441c1bd2918f0660cad8a56f12e
Sean Osborne <soinnovation.oma@gmail.com>
2018-09-06 22:50:30 -0500

    Pushing up

---
+++ MyFirstApp/app/src/main/java/com/soinnovation/myfirstapp/MyActivity.java
@@ -0,0 +1,63 @@
+package com.soinnovation.myfirstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            // Tell Android that the item click has been handled.
+            openSettings();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void openSearch(){
+        Intent intent = new Intent(this, DisplaySearchActivity.class);
+        startActivity((intent));
+    }
+
+    public void openSettings(){
+        Intent intent = new Intent(this, DisplaySettingsActivity.class);
+        startActivity(intent);
+    }
+}
+
+
+
